d1fc989d9277411a9c78c0ed194617b5b50e9d91
DiegoMoreno0640 <diegomoreno.pro45@gmail.com>
2022-03-14 08:18:19 +0000

    init: ReactJS

---
+++ client/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Example from './components/testcomponent'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Example></Example>
+      </div>
+      
+    );
+  }
+}
+
+export default App;
